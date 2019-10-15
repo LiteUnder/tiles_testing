a07521d8360f430bf45da47fe3f41649f9ed6f28
LiteUnder <alxjacobs14@gmail.com>
2019-10-14 18:01:39 -0600

    Added imgui debug value for speed

--- src/game/systems/imgui_window.rs
+++ src/game/systems/imgui_window.rs
@@ -1,14 +1,25 @@
 use amethyst::ecs::prelude::*;
 use amethyst_imgui;
+use amethyst_imgui::imgui::*;
 
+use crate::game::components::Player;
 #[derive(Default, Clone, Copy)]
 pub struct ImguiWindow;
 impl<'s> System<'s> for ImguiWindow {
-    type SystemData = ();
+    type SystemData = (
+        WriteStorage<'s, Player>,
+    );
 
-    fn run(&mut self, _: Self::SystemData) {
-        amethyst_imgui::with(|ui| {
-            ui.show_demo_window(&mut true);
-        });
+    fn run(&mut self, (mut players,): Self::SystemData) {
+        for player in (&mut players).join() {
+            amethyst_imgui::with(|ui| {
+                Window::new(im_str!("Debug"))
+                    .build(ui, || {
+                        ui.text("Player speed");
+
+                        Slider::<f32>::new(im_str!("Player speed"), 0.0..=1000.0).build(ui, &mut player.speed);
+                    })
+            });
+        }
     }
 }
